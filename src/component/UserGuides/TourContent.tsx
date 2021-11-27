8baac5d48533013e4fab3d84ad6e70005404d9f7
wangdongsheng021 <wangdongsheng021@ke.com>
2021-11-28 02:33:12 +0800

    pc引导

--- src/component/UserGuides/TourContent.tsx
+++ src/component/UserGuides/TourContent.tsx
@@ -1,15 +1,16 @@
 // @ts-nocheck
-import React  from 'react'
-import { TourNavigation } from './TourNavigation'
+import React from 'react';
+import { TourNavigation } from './TourNavigation';
 
+import { Popover, Button, Toast } from 'antd-mobile'
 interface TourContentProps {
-  step: number
-  total: number
-  inDOM: boolean
-  goTo: (step: number) => void
-  close: () => void
-  style: any
-  className: string
+  step: number;
+  total: number;
+  inDOM: boolean;
+  goTo: (step: number) => void;
+  close: () => void;
+  style: any;
+  className: string;
 }
 
 export const TourContent: React.FC<TourContentProps> = ({
@@ -17,6 +18,7 @@ export const TourContent: React.FC<TourContentProps> = ({
   goTo,
   inDOM,
   step,
+  placement = 'top',
   total,
   children,
   style,
@@ -24,18 +26,44 @@ export const TourContent: React.FC<TourContentProps> = ({
 }) => {
   return (
     <>
-      <div className="tourContent-box">
-      <div className={className}  style={{ ...style }}>
-      {children}
+  <div id="tourContent-box" className="tourContent-box">          
+  <Popover
+        content={children}
+        getContainer = {() => document.getElementById('tourContent-box')}
+        visible
+        mode={'dark'}
+        placement={placement}>
+        <div>
+          <div className="pointer-nav" onClick={() => goTo(step)}>
+            <div className="round-point"></div>
+          </div>
+        </div>
+      </Popover>
       <TourNavigation step={step} goTo={goTo} total={total} close={close} />
-     </div>
-     <div className="out">
-          {/* <div className="in"></div> */}
-    </div>
-    </div>
-      <div className="pointer-nav" onClick={() => goTo(step)}>
-        <div className='round-point'></div>
-    </div>
+
+</div>
+      {/* <div className="tourContent-box" id="tourContent-box">
+       <div className={className} style={{ ...style }}>
+          {children}
+        <TourNavigation step={step} goTo={goTo} total={total} close={close} />
+      </div>
+        <div className="out"><div className="in"></div></div>
+        <div id="popover-tips"
+          className="popover-tips"
+        >
+          <span className="popover-in"></span>
+        </div>
+      
+      <div>
+          <div className="pointer-nav" onClick={() => goTo(step)}>
+            <div className="round-point"></div>
+          </div>
+        </div> */}
+    
+    
+      {/* </div>  */}
+      
+      
     </>
-  )
-}
+  );
+};
