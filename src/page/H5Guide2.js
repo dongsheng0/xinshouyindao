ca316c025ef14e6e204624cce734bdcc045f2020
wangdongsheng021 <wangdongsheng021@ke.com>
2021-11-28 20:27:50 +0800

    class

--- src/page/H5Guide2.js
+++ src/page/H5Guide2.js
@@ -1,7 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
 import { H5GuideMain2 } from "../component/H5GuideMain2";
 function GuidePc(params) {
+  useEffect(() => {
+    document.body.removeAttribute('class', 'pc-body')
+    document.body.setAttribute('class', 'h5-body')
+    return () => {
+      document.body.removeAttribute('class', 'h5-body')
+    }
+  }, [])
   return (
     <>
       <div className='home-h5'>
--- src/page/PcGuide.js
+++ src/page/PcGuide.js
@@ -1,7 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
 import { PCGuideMain } from "../component/PCGuideMain";
 function GuidePc(params) {
+  useEffect(() => {
+    document.body.removeAttribute('class', 'h5-body')
+    document.body.setAttribute('class', 'pc-body')
+    return () => {
+      document.body.removeAttribute('class', 'pc-body')
+    }
+  }, [])
   return (
     <>
       <div className='home-pc'>
