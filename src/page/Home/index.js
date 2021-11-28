0a5c7b5fcbdfe37c2354df8244a353eb72c2515f
wangdongsheng021 <wangdongsheng021@ke.com>
2021-11-29 02:35:58 +0800

    h5

--- src/page/Home/index.js
+++ src/page/Home/index.js
@@ -1,5 +1,14 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect ,} from 'react'
 import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
+import { Redirect, useHistory, useLocation, useRouteMatch } from 'react-router'
+
+function goPAGE() {
+  if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+    return true
+  } else {
+    return false
+  }
+}
 function Home(params) {
   useEffect(() => {
     document.body.removeAttribute('class', 'pc_guide_body')
@@ -8,10 +17,21 @@ function Home(params) {
       document.body.removeAttribute('class', 'h5_guide_body')
     }
   }, [])
+
+  const history = useHistory()
+  useEffect(() => {
+    if(goPAGE()) {
+      history.push('/home_h5')
+
+    } else {
+      history.push('/home_pc')
+    }
+  }, [])
+
   return (
     <>
       <div className='home'>
-          home
+          
       </div>
     </>
   )
