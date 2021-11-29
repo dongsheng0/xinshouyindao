7753c0d4aef8cd2b041bfdbc74ae7593686ed70f
wangdongsheng021 <wangdongsheng021@ke.com>
2021-11-29 16:20:14 +0800

    滚动到底部

--- src/component/H5GuideMain1/TourNavigation.tsx
+++ src/component/H5GuideMain1/TourNavigation.tsx
@@ -1,5 +1,9 @@
 // @ts-nocheck
-import React from 'react'
+import React, {useEffect} from 'react'
+import ReactDOM from 'react-dom'
+import { Badge, TabBar } from 'antd-mobile'
+import { LeftOutline, RightOutline } from 'antd-mobile-icons'
+
 
 interface TourNavigationProps {
   total: number
@@ -8,27 +12,54 @@ interface TourNavigationProps {
   close: () => void
 }
 
+const tabs = [{
+    key: 'pre',
+    title: '上一步',
+  },
+  {
+    key: 'all',
+    title: '选择流程',
+  },
+  {
+    key: 'next',
+    title: '下一步',
+  },
+]
+
 export const TourNavigation: React.FC<TourNavigationProps> = ({
   step,
   total,
   goTo,
   close,
-}) => (
-  <div className="buttons">
-    {/* {step > 1 && (
-      <div className="button" onClick={() => goTo(step - 2)}>
-        上一步
-      </div>
-    )}
-    {step < total && (
-      <div className="button" onClick={() => goTo(step)}>
-        下一步
-      </div>
-    )}
-    {step === total && (
-      <div className="button" onClick={close}>
-        完成
-      </div>
-    )} */}
-  </div>
-)
+}) => {
+  
+  return ReactDOM.createPortal(
+       <div className="buttons">
+         {step > 1 ? (
+          <div className="button button-prev" onClick={() => goTo(step - 2)}>
+            <LeftOutline /> 上一步
+          </div>
+        ): 
+        <div className="button button-prev disabled">
+         <LeftOutline />  上一步
+        </div>
+        }
+
+        <div className="button">
+            切换场景
+        </div>
+
+        {step < total && (
+          <div className="button button-next"  onClick={() => goTo(step)}>
+            下一步 <RightOutline />
+          </div>
+        )}
+        {step === total && (
+          <div className="button button-next" onClick={close}>
+            完成
+          </div>
+        )} 
+  </div>,
+    document.body
+  )
+}
