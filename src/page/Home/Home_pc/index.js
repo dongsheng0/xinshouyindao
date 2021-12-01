a8c2b4c1398a6cbb75e272742e1b0829a812420c
wangdongsheng021 <wangdongsheng021@ke.com>
2021-12-01 17:50:55 +0800

    修改首页

--- src/page/Home/Home_pc/index.js
+++ src/page/Home/Home_pc/index.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 
 import "./index.scss"
 import ewm from './img/ewm.png'
+import qunerwei from './img/qunerwei.jpg'
 const h5_guide_1_url =  '/#/h5_guide_1?from=pcIframe'
 const h5_guide_2_url =  '/#/h5_guide_2?from=pcIframe'
 function HomePc (params) {
@@ -83,6 +84,32 @@ function HomePc (params) {
                         2.使用APP端体验快速启动盖印操作，可直接查看操作者当前允许启动物联章桶盖印的申请单，并直接对物联章桶进行开始盖章、暂停盖章、完成盖章
                       </div>
                     </div>
+                    <div className="info">
+                      <div className="small" style={{fontSize: '20px'}}>场景体验</div>
+                      <div className="gap"></div>
+                      <div className="big">智能印章管理</div>
+                      <div className="english">让印章申请流程更简单！</div>
+                      <div className="gap"></div>
+                      <div className="small">体验效果：</div>
+                      <div className="small">
+                        1.通过智能印章APP端体验一次用章申请的全流程，从申请发起到审批，到印章使用，再到后期的盖章文件归档流程
+                      </div>
+                      <div className="small">
+                        2.使用APP端体验快速启动盖印操作，可直接查看操作者当前允许启动物联章桶盖印的申请单，并直接对物联章桶进行开始盖章、暂停盖章、完成盖章
+                      </div>
+                      <div className="small" style={{width: '330px', paddingTop:'30px'}}>
+                        <div style={{width:'195px',  overflow:'hidden', float:'left'}}>
+                          <img src={qunerwei} style={{width:'80px', paddingBottom:'10px'}} alt="" /><br />
+                          扫描二维码，加入群聊，<br />了解更多！
+                        </div>
+                        <div style={{width: '130px',  float:'left', overflow:'hidden'}}>
+                          产品咨询：<br />
+                          韩天阔：18533663028<br />
+                          回　昆：0316-2599967
+                        </div>
+                      </div>
+                    </div>
+
               </div>
             </div>
           </div>
